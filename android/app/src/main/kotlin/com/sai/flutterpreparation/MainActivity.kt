d88aa22ba5717cbceef46ec66abab872006a70cd
Sai Madhav Chenna <sai.madhav@applaunch.io>
2022-12-27 21:37:52 +0530

    initial commit

---
+++ android/app/src/main/kotlin/com/sai/flutterpreparation/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.sai.flutterpreparation
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Center(
+      child: Text("Sample App", textDirection: TextDirection.ltr)));
+}
