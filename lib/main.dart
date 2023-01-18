fa11b1ba7679746a102502a826b526288c0de0b1
Sai Madhav Chenna <sai.madhav@applaunch.io>
2023-01-18 21:00:14 +0530

    sample app learn

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,41 @@
 import 'package:flutter/material.dart';
-import 'package:flutterpreparation/samplescreenui/login_screen.dart';
 import 'package:flutterpreparation/samplescreenui/with_tabbar.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(MaterialApp(
+      title: "Sample App",
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primaryColor: Colors.blue, brightness: Brightness.dark),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Main Page"),
+          titleTextStyle: TextStyle(fontStyle: FontStyle.italic),
+          centerTitle: true,
+          backgroundColor: Colors.grey,
+        ),
+        body: Container(
+          child: Center(
+            child: Text("Flutter",
+                textDirection: TextDirection.ltr,
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    fontStyle: FontStyle.italic,
+                    fontWeight: FontWeight.w300,
+                    fontSize: 20,
+                    color: Colors.red[200],
+                    fontFamily: "Raleway")),
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          backgroundColor: Colors.white,
+          child: Icon(
+            Icons.add,
+            color: Colors.black,
+          ),
+        ),
+        backgroundColor: Colors.blueGrey,
+      )));
 }
 
 class MyApp extends StatefulWidget {
@@ -20,30 +52,7 @@ class _MyAppState extends State<MyApp> {
         brightness: Brightness.dark,
       ),
       debugShowCheckedModeBanner: false,
-      home: WithTabBar (),
+      home: WithTabBar(),
     );
-  }
-}
-
-class SampleApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-        title: "SampleApp",
-        home: Scaffold(
-            appBar: AppBar(
-              title: const Text("SampleApp",
-                  style:
-                      TextStyle(fontStyle: FontStyle.italic, fontSize: 20.0)),
-              backgroundColor: Colors.black12,
-            ),
-            body: LoginScreen(),
-            floatingActionButton: FloatingActionButton(
-              onPressed: () {
-                debugPrint("FAB clicked");
-              },
-              tooltip: 'Add One More Item',
-              child: const Icon(Icons.add),
-            )));
   }
 }
